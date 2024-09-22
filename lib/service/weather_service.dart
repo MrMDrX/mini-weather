36014c1dca08e53e6b8c8ec946f64205d3e32b3c
Med <geek.4tech007@gmail.com>
2024-09-22 15:01:19 +0000

    Add Weather model & service ✨

---
+++ lib/service/weather_service.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:mini_weather/models/weather_model.dart';
+
+class WeatherService {
+  final String apiKey;
+  WeatherService({required this.apiKey});
+  static const String _baseUrl =
+      'https://api.openweathermap.org/data/2.5/weather';
+
+  Future<Weather> getWeather(String city) async {
+    final Uri url =
+        Uri.parse('$_baseUrl/weather?q=$city&apiKey=$apiKey&units=metric');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return Weather.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Location permissions are permanently denied');
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        locationSettings:
+            const LocationSettings(accuracy: LocationAccuracy.high));
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
